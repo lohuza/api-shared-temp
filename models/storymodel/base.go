11a2e354fefa0f30d72090479fb4570388bf6587
Lasha <lasha.abazashvili@gmail.com>
2024-10-25 06:11:11 +0400

    modified story

--- models/storymodel/base.go
+++ models/storymodel/base.go
@@ -1,13 +1,14 @@
 package storymodel
 
 type Story struct {
-	ID          string  `json:"id"`
-	Type        string  `json:"type"`
-	Title       string  `json:"title"`
-	Image       string  `json:"image"`
-	PlaybackID  string  `json:"playback_id"`
-	Duration    float32 `json:"duration"`
-	Thumbnail   string  `json:"thumbnail"`
-	ButtonTitle string  `json:"button_title"`
-	ButtonURL   string  `json:"url"`
+	ID             string  `json:"id"`
+	Type           string  `json:"type"`
+	Title          string  `json:"title"`
+	Image          string  `json:"image"`
+	PlaybackID     string  `json:"playback_id"`
+	Duration       float32 `json:"duration"`
+	Thumbnail      string  `json:"thumbnail"`
+	ButtonTitle    string  `json:"button_title"`
+	ButtonURL      string  `json:"url"`
+	HighlightImage string  `json:"highlight_image"`
 }
